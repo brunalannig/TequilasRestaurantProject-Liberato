2f461ebea084366dc3f5d0564d2d104094ecad0c
Bruna Lânnig <lannigbrunah@gmail.com>
2025-03-23 21:34:39 -0300

    update of database

---
+++ TequilasRestaurant/Models/ProductIngredient.cs
@@ -0,0 +1,11 @@
+﻿namespace TequilasRestaurant.Models
+{
+    public class ProductIngredient
+    {
+        //join table
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public int IngredientId { get; set; }
+        public Ingredient Ingredient { get; set; }
+    }
+}
